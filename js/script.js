a749f8c2e83f842b60ec023ffaf3660a655918be
wintermute28 <a28c@yandex.ru>
2023-08-30 17:28:02 +0300

    Add trim()

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ let personalMovieDB = {
 
 function rememberMyFilms() {
 	for (let i = 0; i < 2; i++) {
-		const a = prompt("Один из последних просмотренных фильмов?", ""),
+		const a = prompt("Один из последних просмотренных фильмов?", "").trim(),
 			b = prompt("На сколько оцените его?", "");
 		if (a != null && b != null && a != "" && b != "" && a.length < 50) {
 			personalMovieDB.movies[a] = b;
